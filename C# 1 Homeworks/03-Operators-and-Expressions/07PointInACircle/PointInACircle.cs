ca60764d2f4b09e7f1f908455e4d1b19f8f110af
grandeto.com <grandeto.com@gmail.com>
2016-04-08 11:39:40 +0300

    change

--- C# 1 Homeworks/03-Operators-and-Expressions/07PointInACircle/PointInACircle.cs
+++ C# 1 Homeworks/03-Operators-and-Expressions/07PointInACircle/PointInACircle.cs
@@ -10,6 +10,42 @@ namespace _07PointInACircle
     {
         static void Main(string[] args)
         {
+            double radius = 4;
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double minXY = -1000;
+            double maxXY = 1000;
+
+                if(x < minXY)
+                {
+                    x = minXY;
+                }
+                if(x > maxXY)
+                {
+                    x = maxXY;
+                }
+                if (y < minXY)
+                {
+                    y = minXY;
+                }
+                if (y > maxXY)
+                {
+                    y = maxXY;
+                }
+
+            double position = ((x * x) + (y * y));
+            double distance = Math.Sqrt(position);
+            string result = string.Format("{0:0.00}", distance);
+
+                if(position <= radius)
+                {
+                    Console.WriteLine("yes {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine("no {0}", result);
+                }
+
         }
     }
 }
