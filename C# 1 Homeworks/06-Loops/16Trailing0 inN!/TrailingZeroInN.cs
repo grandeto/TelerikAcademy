2776b86459131183f43b96b73e156328985849d8
grandeto.com <grandeto.com@gmail.com>
2016-04-23 22:08:24 +0300

    change

--- C# 1 Homeworks/06-Loops/16Trailing0 inN!/TrailingZeroInN.cs
+++ C# 1 Homeworks/06-Loops/16Trailing0 inN!/TrailingZeroInN.cs
@@ -11,14 +11,57 @@ namespace _16Trailing0_inN_
     {
         static void Main()
         {
-            BigInteger N = BigInteger.Parse(Console.ReadLine());
-            BigInteger factorial = 0;
+            int N = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
 
-            for (int i = 0; i <= N; i++)
+            for (int i = 1; i <= N; i++)
             {
                 factorial *= i;
             }
 
+            string fToString = factorial.ToString();
+            int result = 0;
+
+            for (int j = fToString.Length - 1; j > 0; j--)
+            {
+                
+                if (fToString[j] == '0')
+                {
+                    result++;
+                }
+                if (result != 0 && fToString[j] != '0')
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
+
+//the fastest way
+//using System;
+
+//class Program
+//{
+//    public static int countZeros(int n)
+//    {
+
+//        int counter = 0;
+
+//        while (n > 0)
+//        {
+//            n /= 5;
+//            counter += n;
+//        }
+//        return counter;
+//    }
+//    static void Main()
+//    {
+//        int n = int.Parse(Console.ReadLine());
+
+//        int zeros = countZeros(n);
+//        Console.WriteLine(zeros);
+
+//    }
+//}
