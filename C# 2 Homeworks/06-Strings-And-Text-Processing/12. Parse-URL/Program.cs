1bfd94076929c506a901a29c94423a7e5599580a
grandeto.com <grandeto.com@gmail.com>
2016-05-27 17:38:04 +0300

    change

---
+++ C# 2 Homeworks/06-Strings-And-Text-Processing/12. Parse-URL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Parse_URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = Console.ReadLine();
+            Console.WriteLine(ParseUrl(url));
+        }
+
+        private static string ParseUrl(string url)
+        {
+            var parsedUrl = new StringBuilder();
+
+            var outputFormat = "[protocol] = {0}\n[server] = {1}\n[resource] = {2}";
+
+            var endProtocolIndex = url.IndexOf("://");
+            var protocol = url.Substring(0, endProtocolIndex);
+            var startserverIndex = endProtocolIndex + 3;
+            var endServerIndex = url.IndexOf("/", endProtocolIndex + 3);
+            var server = url.Substring(startserverIndex, endServerIndex - startserverIndex);
+            var resourse = url.Substring(endServerIndex);
+
+            parsedUrl.AppendFormat(outputFormat, protocol, server, resourse);
+
+            return parsedUrl.ToString();
+        }
+    }
+}
